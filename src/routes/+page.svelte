509079bb0c0f0373ab418b4bd8748348e6e7271a
True Merrill <truemerrill@gmail.com>
2025-05-14 14:20:28 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+   import { signIn } from "@auth/sveltekit/client"
+</script>
+ 
+<div>
+  <img src="/img/logo.svg" alt="Company Logo" />
+  <button onclick={() => signIn('github')} aria-label="login" >Login</button>
+</div> 
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
